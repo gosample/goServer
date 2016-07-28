902e6890f321123068e39b58fb22a87428eeec3d
will <chenweimin74@gmail.com>
2016-07-28 10:37:04 +0800

    fix bug

--- routers/commentsRouter_parkinglots_controllers.go
+++ routers/commentsRouter_parkinglots_controllers.go
@@ -69,6 +69,13 @@ func init() {
 			[]string{"get"},
 			nil})
 
+	beego.GlobalControllerRouter["parkinglots/controllers:ServiceController"] = append(beego.GlobalControllerRouter["parkinglots/controllers:ServiceController"],
+		beego.ControllerComments{
+			"GetHistoryService",
+			`/history`,
+			[]string{"get"},
+			nil})
+
 	beego.GlobalControllerRouter["parkinglots/controllers:UserController"] = append(beego.GlobalControllerRouter["parkinglots/controllers:UserController"],
 		beego.ControllerComments{
 			"Register",
@@ -102,13 +109,6 @@ func init() {
 			"AddCarLicense",
 			`/addcarlicense`,
 			[]string{"post"},
-			nil})
-
-	beego.GlobalControllerRouter["parkinglots/controllers:ServiceController"] = append(beego.GlobalControllerRouter["parkinglots/controllers:ServiceController"],
-		beego.ControllerComments{
-			"GetHistoryService",
-			`/history`,
-			[]string{"get"},
 			nil})
 
 }
